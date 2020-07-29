4c5a9475d54dcd3fa76125060dc9a201c23c208c
Mohit <moh23it07@gmail.com>
2020-07-29 09:28:27 +0530

    Modified forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (latitude,longitutde, callback)=>{
-    const url = 'http://api.weatherstack.com/current?access_key=56ce467b6b3cc7dda7a1058f2646a4ef&query='+ encodeURIComponent(latitude)+','+encodeURIComponent(longitutde)+'&units=f';
+    const url = 'http://api.weatherstack.com/current?access_key=56ce467b6b3cc7dda7a1058f2646a4ef&query='+ encodeURIComponent(latitude)+','+encodeURIComponent(longitutde)+'&units=m';
 
     request({url, json: true}, (error,{body})=>{
         if(error)
@@ -17,7 +17,7 @@ const forecast = (latitude,longitutde, callback)=>{
             var temp = body.current.temperature;
             var feelsLike = body.current.feelslike;
         
-            callback(undefined,body.current.weather_descriptions[0]+'. The temperature is '+ temp + ' degrees but it feels like '+ feelsLike + ' degrees outside.')
+            callback(undefined,`${body.current.weather_descriptions[0]}. The temperature is ${temp} degree celsius, but it feels like ${feelsLike} degree celsius outside. The humidity is ${body.current.humidity}% and the visibility is ${body.current.visibility}. `)
         }
     })
 };
